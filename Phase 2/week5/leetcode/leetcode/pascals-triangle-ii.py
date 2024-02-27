792a6b14dddf168816416933835e3700c032c782
naodmulu <100387620+naodmulu@users.noreply.github.com>
2024-02-27 14:22:22 +0300

    Add solution for Pascal's Triangle II

--- Phase 2/week5/leetcode/leetcode/pascals-triangle-ii.py
+++ Phase 2/week5/leetcode/leetcode/pascals-triangle-ii.py
@@ -6,14 +6,13 @@ class Solution:
             return [1]
         elif rowIndex == 1:
             return [1,1]
-        else:
-            row = self.getRow(rowIndex-1)
-            arr = [1]
-            for i in range(1,len(row)):
-                arr.append(row[i]+row[i-1])
-            arr.append(1)
-            
-            return arr
+        row = self.getRow(rowIndex-1)
+        arr = [1]
+        for i in range(1,len(row)):
+            arr.append(row[i]+row[i-1])
+        arr.append(1)
+
+        return arr
 
 
         
