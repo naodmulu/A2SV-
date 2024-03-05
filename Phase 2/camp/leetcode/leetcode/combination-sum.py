d3148bac99770a7fe72086c4e078f0dd26b16661
naodmulu <100387620+naodmulu@users.noreply.github.com>
2024-03-05 14:18:11 +0300

    Add solution for Combination Sum

--- Phase 2/camp/leetcode/leetcode/combination-sum.py
+++ Phase 2/camp/leetcode/leetcode/combination-sum.py
@@ -4,16 +4,14 @@ class Solution:
         path = []
         def helper(idx,path,total):
             if total == target:
-                ans.add(tuple(path[:]))
+                ans.add(tuple(path))
                 return 
             if total > target:
                 return
             
             for i in range(idx,len(candidates)):
-                path.append(candidates[i])
                 total += candidates[i]
-                helper(i,path,total)
-                path.pop()
+                helper(i,path + [candidates[i]],total)
                 total -= candidates[i]
         helper(0,path,0)
         return ans
